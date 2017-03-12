dffa784644fc8a0f3c8e40ee60a28ec53690dc38
bn5092017 <inkspotless@gmail.com>
2017-03-12 18:57:10 +0000

    V1: User class and database access function

--- public/index.php
+++ public/index.php
@@ -21,3 +21,14 @@ function connect_to_db()
     }
     return $conn;
 }
+
+$u = new \Itb\User();
+$u->getAllUsers();
+
+while($row = $u->getAllUsers())
+{
+    echo $row['username'] . PHP_EOL;
+    echo $row['firstName'] . PHP_EOL;
+    echo $row['lastName'] . PHP_EOL;
+    echo $row['role'] . PHP_EOL;
+}
--- src/User.php
+++ src/User.php
@@ -12,8 +12,36 @@ namespace Itb;
  */
 class User
 {
-    function __construct()
+    public function connect_to_db()
     {
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
+        try
+        {
+            $conn = new \PDO($dsn, DB_USER, DB_PASS);
+        }
+        catch (\PDOException $e)
+        {
+            print 'There was an error connecting to the database';
+            return null;
+        }
+        return $conn;
+    }
 
+    public function getAllUsers()
+    {
+        $connection = $this->connect_to_db();
+        if(null !=$connection)
+        {
+            print 'Success - connection to database created' . PHP_EOL;
+        }else{
+            print 'There was an error connecting to the database'. PHP_EOL;
+        }
+
+        $sql = 'SELECT * FROM user';
+
+        $statement = $connection->query($sql);
+        $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $statement;
     }
 }
