7a41682b71b56c413007660398d0afd7213e2cbf
bn5092017 <inkspotless@gmail.com>
2017-03-18 17:32:02 +0000

    V1: Constructor for User class
    Added and tested a constructor and a getOneUser method for the User
    class.  Adding the constructor caused failures in the other tests.
    Removed testing db connection lines in getAllUsers method.

--- src/User.php
+++ src/User.php
@@ -12,7 +12,7 @@ namespace Itb;
  */
 class User
 {
-    public function connect_to_db()
+    public function connectToDb()
     {
         $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME;
         try
@@ -27,20 +27,53 @@ class User
         return $conn;
     }
 
+    public function __construct($id, $username, $password, $firstName, $lastName, $role, $status, $image)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->role = $role;
+        $this->status = $status;
+        $this->image = $image;
+
+        $connection = $this->connectToDb();
+        $sql = $connection->prepare('INSERT INTO user (id, username, password, firstName, lastName, role, status, image) VALUES (:id, :username, :password, :firstName, :lastName, :role, :status, :image)');
+
+        $sql->execute([':id' => $id,
+            ':username' => $username,
+            ':password' => $password,
+            ':firstName' => $firstName,
+            ':lastName' => $lastName,
+            ':role' => $role,
+            ':status' => $status,
+            ':image' => $image]);
+        $rows = $sql->rowCount();
+
+        print $rows . ' rows affected';
+    }
+
     public function getAllUsers()
     {
-        $connection = $this->connect_to_db();
-        if(null !=$connection)
-        {
-            print 'Success - connection to database created' . PHP_EOL;
-        }else{
-            print 'There was an error connecting to the database'. PHP_EOL;
-        }
+        $connection = $this->connectToDb();
 
         $sql = 'SELECT * FROM user';
 
         $statement = $connection->query($sql);
         $statement->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $statement;
+    }
+
+    public function getOneUser($username)
+    {
+        $connection = $this->connectToDb();
+
+        $sql = $connection->prepare('SELECT * FROM user WHERE username=:username');
+
+        $sql->execute([':username' => $username]);
+        $statement = $sql->fetch(\PDO::FETCH_ASSOC);
 
         return $statement;
     }
